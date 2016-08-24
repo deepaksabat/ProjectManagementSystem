5e1a5122d4eb54d176c9fc74020400af7f3149f7
cassaram09 <cassaram09@gmail.com>
2016-08-24 19:12:05 -0400

    expand all active_tasks handlebars template and tasks.js, add assigned_users to tasks serializer

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -5,14 +5,45 @@ function getActiveTasks() {
       method: "GET",
       dataType: 'JSON'
     }).success(function(data) {
+        $(".row").html("");
         $.each(data, function(index, task){
-          $(".row").append(task.description);
+          var task = new Task(task);
+          var taskRender = task.renderTask()
+          $(".row").prepend(taskRender);
         })
       });
   });
 }
 
+function Task(attributes) {
+  this.id = attributes.id;
+  this.description = attributes.description;
+  this.name = attributes.name;
+  this.dueDate = attributes.due_date;
+  this.status = attributes.status;
+  this.createdAt = attributes.created_at;
+  this.ownerName = attributes.owner.name;
+  this.projectName = attributes.project.name;
+  this.commentCount = attributes.comments.length;
+  this.projectID = attributes.project.id;
+  this.assignees = assignUsers();
+  debugger
+
+  function assignUsers() {
+    var names = "";
+    $.each(attributes.assigned_users, function(index, user){
+      names = names + user.name + " ";
+    });
+    return names;
+  }
+}
+
+Task.prototype.renderTask = function (){
+  return Task.template(this);
+}
 
 $(document).ready(function(){
+  Task.templateSource = $("#activeTasksTemplate").html();
+  Task.template = Handlebars.compile(Task.templateSource); 
   getActiveTasks();
 });
--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,4 +1,4 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :due_date, :status, :created_at, :updated_at, :owner, :comments
+  attributes :id, :name, :description, :due_date, :status, :created_at, :updated_at, :owner, :comments, :assigned_users
   has_one :project
 end
