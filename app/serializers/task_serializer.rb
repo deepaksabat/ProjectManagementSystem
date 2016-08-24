50e2dfb49f5d949125635db5bd414b6fc57ae376
cassaram09 <cassaram09@gmail.com>
2016-08-24 18:02:31 -0400

    add owner info to task serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,4 +1,4 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :owner_id, :name, :description, :due_date, :status, :created_at, :updated_at
+  attributes :id, :name, :description, :due_date, :status, :created_at, :updated_at, :owner, :comments
   has_one :project
 end
