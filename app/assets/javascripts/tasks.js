607e88a5c5072aa4e2dfe23d501fab6e7a1fefdd
cassaram09 <cassaram09@gmail.com>
2016-08-25 23:12:16 -0400

    refactor tasks object and prototype into class in tasks.js

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,3 +1,46 @@
+class Task {
+  constructor(attributes){
+    this.id = attributes.id;
+    this.description = attributes.description;
+    this.name = attributes.name;
+    this.dueDate = attributes.due_date;
+    this.status = attributes.status;
+    this.createdAt = attributes.created_at;
+    this.ownerName = attributes.owner.name;
+    this.projectName = attributes.project.name;
+    this.commentCount = attributes.comments.length;
+    this.projectID = attributes.project.id;
+    this.overdue = "";
+    this.complete = "";
+  }
+
+  overdueCheck() {
+    var createdDate = new Date(this.createdAt);
+    var currentDate = new Date();
+    if (createdDate < currentDate) {
+      this.overdue = "Overdue";
+    }
+  }
+
+  completeCheck() {
+    if (this.status === "complete") {
+      this.complete = "Complete";
+    }
+  }
+
+  renderTask() {
+    return Task.template(this);
+  }
+
+  assignUsers() {
+    var names = "";
+    $.each(attributes.assigned_users, function(index, user){
+      names = names + user.name + " ";
+    });
+    return names;
+  }
+}
+
 function getActiveTasks() {
   $('#active-tasks').on("click", function(e) {
     $.ajax({
@@ -15,49 +58,6 @@ function getActiveTasks() {
         })
       });
   });
-}
-
-function Task(attributes) {
-  this.id = attributes.id;
-  this.description = attributes.description;
-  this.name = attributes.name;
-  this.dueDate = attributes.due_date;
-  this.status = attributes.status;
-  this.createdAt = attributes.created_at;
-  this.ownerName = attributes.owner.name;
-  this.projectName = attributes.project.name;
-  this.commentCount = attributes.comments.length;
-  this.projectID = attributes.project.id;
-  this.overdue = "";
-  this.complete = "";
-}
-
-Task.prototype.assignUsers = function() {
-  var names = "";
-  $.each(attributes.assigned_users, function(index, user){
-    names = names + user.name + " ";
-  });
-  return names;
-}
-
-Task.prototype.overdueCheck = function() {
-  var createdDate = new Date(this.createdAt);
-  var currentDate = new Date();
-  if (createdDate < currentDate) {
-    this.overdue = "Overdue";
-  } else {
-    return "hello";
-  }
-}
-
-Task.prototype.completeCheck = function() {
-  if (this.status === "complete") {
-    this.complete = "Complete";
-  }
-}
-
-Task.prototype.renderTask = function (){
-  return Task.template(this);
 }
 
 $(document).ready(function(){
