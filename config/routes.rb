f237acb722d61ccd9dc22ea8a39f507b023aaf51
cassaram09 <cassaram09@gmail.com>
2016-08-29 12:20:42 -0400

    consolidate task routes into /all

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,7 @@ Rails.application.routes.draw do
   scope "/tasks" do
     get'/new', to: 'tasks#quick_new_task', as: "quick_new_task"
     post'/', to: 'tasks#create', as: "post_quick_new_task"
-    get'/all-active-tasks', to: 'tasks#all_active_tasks', as: "all_active_tasks"
-    get'/all-complete-tasks', to: 'tasks#all_complete_tasks', as: "all_complete_tasks"
-    get'/all-overdue-tasks', to: 'tasks#all_overdue_tasks', as: "all_overdue_tasks"
+    get'/all', to: 'tasks#all', as: "get_all_tasks"
   end
 
   resources :projects do
