f3370a490c4f15a8c43be503183a4e9cbd704ad0
cassaram09 <cassaram09@gmail.com>
2016-09-03 15:33:49 -0400

    edit active admin user form

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,18 +1,13 @@
 ActiveAdmin.register User do
 permit_params :role
   actions :all, except: [:destroy]
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
+  form do |f|
+    inputs 'User' do
+      f.input :role
+    end
+    f.semantic_errors
+    f.actions
+  end
 
 end
