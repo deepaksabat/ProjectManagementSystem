a26582dfde06399a01fcc390ac4e38e65d73beee
cassaram09 <cassaram09@gmail.com>
2016-09-14 12:00:34 -0400

    remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,22 +21,13 @@ Rails.application.routes.draw do
   scope "/tasks" do
     get'/new', to: 'tasks#quick_new_task', as: "quick_new_task"
     post'/', to: 'tasks#create', as: "post_quick_new_task"
-    get'/all-active-tasks', to: 'tasks#all_active_tasks', as: "all_active_tasks"
-    get'/all-complete-tasks', to: 'tasks#all_complete_tasks', as: "all_complete_tasks"
-    get'/all-overdue-tasks', to: 'tasks#all_overdue_tasks', as: "all_overdue_tasks"
     get'/all', to: 'tasks#all', as: "get_all_tasks"
   end
 
   resources :projects do
-
     post '/delete_collaborator', to: 'projects#delete_collaborator'
-
     resources :comments, :notes, shallow: true
-
-    resources :tasks, shallow: true do 
-      get :complete, on: :collection, to: "tasks#complete"
-      get :overdue, on: :collection, to: "tasks#overdue"
-    end
+    resources :tasks, shallow: true
   end
 
   patch '/edit_user_project_permission', to: 'user_projects#edit_user_project_permission'
